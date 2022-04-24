32fceabc6440b2125886f18373c408c2dd3d3451
Tuncay Demir <tuncaydemir682@gmail.com>
2022-04-25 00:19:20 +0300

    Create Salarycalculator.java

---
+++ Salarycalculator.java
@@ -0,0 +1,9 @@
+public class Salarycalculator {
+    public static void main(String[] args) {
+
+        Employee emp = new Employee("John Smith", 2000, 45, 1985);
+
+        emp.employeeInfo();
+
+    }
+}
